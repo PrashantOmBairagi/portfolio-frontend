fceefc9d24c847326868c74ef00aa909e2a62009
PrashantOmBairagi <prashantbairagi2018@gmail.com>
2026-08-17 16:23:26 +0530

    refact : refactor backend api address

--- Portfolio v2/js/script.js
+++ Portfolio v2/js/script.js
@@ -537,7 +537,7 @@ document.addEventListener('DOMContentLoaded', () => {
             }, 8000);
             
             try {
-                const response = await fetch('https://portfolio-backend-api-onqc.onrender.com/api/contacts', {
+                const response = await fetch('https://43.204.7.243/api/contacts', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json'
